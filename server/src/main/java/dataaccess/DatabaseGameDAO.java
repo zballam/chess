19da4d51a21befe2751e2d146a62dc8ef70f1ef3
Zachary Christopher Ballam <zballam@byu.edu>
2024-10-31 15:48:28 -0600

    Get game method

--- server/src/main/java/dataaccess/DatabaseGameDAO.java
+++ server/src/main/java/dataaccess/DatabaseGameDAO.java
@@ -1,13 +1,19 @@
 package dataaccess;
 
 import chess.ChessGame;
+import com.google.gson.Gson;
 import model.GameData;
 import model.UserData;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Collection;
 import java.util.List;
 
+
 public class DatabaseGameDAO implements GameDAO{
+    private static final Gson GSON = new Gson();
+
     public DatabaseGameDAO() {
         DatabaseManager.configureDatabase();
     }
@@ -20,12 +26,52 @@ public class DatabaseGameDAO implements GameDAO{
 
     @Override
     public int createGame(GameData createRequest) throws DataAccessException {
-        return 0;
+        // Serialize game
+        String gameInfo = GSON.toJson(createRequest.game());
+        String insertStatement = """
+                    INSERT INTO game (gameID, whiteUsername, blackUsername, gameName, game) VALUES (?, ?, ?, ?, ?);
+                    """;
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var ps = conn.prepareStatement(insertStatement)) {
+                ps.setInt(1, createRequest.gameID());
+                ps.setString(2, createRequest.whiteUsername());
+                ps.setString(3, createRequest.blackUsername());
+                ps.setString(4, createRequest.gameName());
+                ps.setString(5, gameInfo);
+                ps.executeUpdate();
+                return createRequest.gameID();
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getMessage());
+        }
     }
 
     @Override
     public GameData getGame(int gameID) throws DataAccessException {
-        return null;
+        String userQuery = """
+                SELECT * FROM game WHERE gameID = ?;
+                """;
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var ps = conn.prepareStatement(userQuery)) {
+                ps.setString(1, String.valueOf(gameID));
+                ResultSet rs = ps.executeQuery();
+                if (rs.next()) {
+                    int dataGameID = rs.getInt(1);
+                    String whiteUsername = rs.getString(2);
+                    String blackUsername = rs.getString(3);
+                    String gameName = rs.getString(4);
+                    String game = rs.getString(5);
+                    ChessGame gameData = GSON.fromJson(game, ChessGame.class);
+                    return new GameData(dataGameID, whiteUsername, blackUsername, gameName, gameData);
+                }
+                else {
+//                    throw new DataAccessException("User doesn't exist");
+                    return null;
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getMessage());
+        }
     }
 
     @Override
