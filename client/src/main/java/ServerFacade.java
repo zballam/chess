3eb4a66fac54c5cd5a091aae45e3b5cb828618c0
Zachary Christopher Ballam <zballam@byu.edu>
2024-11-05 15:17:17 -0700

    Base outline

---
+++ client/src/main/java/ServerFacade.java
@@ -0,0 +1,4 @@
+public class ServerFacade {
+    // This is how the client interacts with the server
+    // This sends and receives server requests
+}
---
+++ client/src/main/java/ui/BoardDrawer.java
@@ -0,0 +1,5 @@
+package ui;
+
+public class BoardDrawer {
+    // This draws the board in the terminal
+}
