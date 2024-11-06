72d95072698070ef2f41fb7ac2901cb6cec2802b
Zachary Christopher Ballam <zballam@byu.edu>
2024-11-05 23:41:43 -0700

    More outlining

--- client/src/main/java/ui/BoardDrawer.java
+++ client/src/main/java/ui/BoardDrawer.java
@@ -1,5 +1,32 @@
 package ui;
 
+import chess.ChessGame;
+import chess.ChessPiece;
+
+import java.util.Collection;
+
 public class BoardDrawer {
     // This draws the board in the terminal
+    public static void main(String[] args) {
+
+    }
+
+    private static void drawChessBoard(Collection<ChessPiece> pieces, ChessGame.TeamColor team) {
+        boolean whiteTeam;
+        if (team == ChessGame.TeamColor.WHITE) { whiteTeam = true; }
+        else { whiteTeam = false; }
+
+    }
+
+    private static void drawHeaders(boolean whiteTeam) {
+
+    }
+
+    private static void drawWhiteSquare() {
+
+    }
+
+    private static void drawBlackSquare() {
+
+    }
 }
