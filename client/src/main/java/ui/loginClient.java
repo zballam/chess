7e2a8d627d60dd798c4cf124867242b7db22a4a7
Zachary Christopher Ballam <zballam@byu.edu>
2024-11-12 14:11:54 -0700

    Outline loginClient methods

--- client/src/main/java/ui/loginClient.java
+++ client/src/main/java/ui/loginClient.java
@@ -5,4 +5,28 @@ public class loginClient {
     public String run(String command) {
         throw new RuntimeException("Not implemented yet");
     }
+
+    public String help(String cmd) {
+        throw new RuntimeException("Not implemented yet");
+    }
+
+    public String logout(String cmd) {
+        throw new RuntimeException("Not implemented yet");
+    }
+
+    public String createGame(String cmd) {
+        throw new RuntimeException("Not implemented yet");
+    }
+
+    public String listGames(String cmd) {
+        throw new RuntimeException("Not implemented yet");
+    }
+
+    public String playGame(String cmd) {
+        throw new RuntimeException("Not implemented yet");
+    }
+
+    public String observeGame(String cmd) {
+        throw new RuntimeException("Not implemented yet");
+    }
 }
