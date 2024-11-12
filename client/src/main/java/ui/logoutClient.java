ab143ba0c21a36681cd82c9ece355f586a22ddd9
Zachary Christopher Ballam <zballam@byu.edu>
2024-11-12 11:50:15 -0700

    UNFINISHED: logoutClient work:

--- client/src/main/java/ui/logoutClient.java
+++ client/src/main/java/ui/logoutClient.java
@@ -1,8 +1,24 @@
 package ui;
 
+import java.util.Arrays;
+
 public class logoutClient {
 
-    public String run (String command) {
-        throw new RuntimeException("Not implemented yet");
+    public String run (String input) {
+        var tokens = input.toLowerCase().split(" ");
+        var cmd = (tokens.length > 0) ? tokens[0] : "help";
+        var params = Arrays.copyOfRange(tokens, 1, tokens.length);
+        try {
+            return switch (cmd) {
+//                case "signin" -> signIn(params);
+//                case "list" -> listPets();
+//                case "signout" -> signOut();
+                case "quit" -> "quit";
+                default -> "quit";//help();
+            };
+        } catch (RuntimeException e) {
+
+        }
+        return input;
     }
 }
