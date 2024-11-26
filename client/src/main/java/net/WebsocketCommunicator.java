8d2884ffccc08165fb7f5390e8e359386fa1f81a
Zachary Christopher Ballam <zballam@byu.edu>
2024-11-25 17:54:50 -0700

    Trying to initialize the websocket classes

--- client/src/main/java/net/WebsocketCommunicator.java
+++ client/src/main/java/net/WebsocketCommunicator.java
@@ -1,11 +1,35 @@
 package net;
 
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import javax.websocket.*;
+
 public class WebsocketCommunicator extends Endpoint {
+
+    Session session;
+
     public WebsocketCommunicator(String url) {
         try {
             url.replace("http","ws");
+            URI socketURI = new URI(url + "/ws");
+
+            WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+            this.session = container.connectToServer(this, socketURI);
+
+            this.session.addMessageHandler(new MessageHandler.Whole<String>() {
+                @Override
+                public void onMessage(String message) {
+                    Notification notification = new Gson().fromJson(message, Notification.class);
+                    notificationHandler.notify(notification);
+                }
+            });
         } catch (DeploymentException | IOException | URISyntaxException ex) {
             throw new ResponseException(500, ex.getMessage());
         }
     }
+
+    public void onOpen(Session session, EndpointConfig endpointConfig) {}
 }
