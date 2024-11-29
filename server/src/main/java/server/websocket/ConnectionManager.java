2ee06110c21ea3ff23cc3a7be2785cce7ce86df4
Zachary Christopher Ballam <zballam@byu.edu>
2024-11-28 19:20:15 -0700

    Update the ConnectionManager class

--- server/src/main/java/server/websocket/ConnectionManager.java
+++ server/src/main/java/server/websocket/ConnectionManager.java
@@ -1,17 +1,21 @@
 package server.websocket;
 
 import java.util.HashMap;
+import java.util.Set;
+
 import org.eclipse.jetty.websocket.api.Session;
 
 public class ConnectionManager {
-    public final HashMap<String, Session> connections = new HashMap<>();
+    public final HashMap<Integer, Set<Session>> connections = new HashMap<>();
 
-    public void add(String playerName, Session session) {
-        connections.put(playerName, session);
+    public void add(Integer gameID, Session session) {
+        connections.get(gameID).add(session);
     }
 
-    public void remove(String playerName) {
-        connections.remove(playerName);
+    public void remove(Integer gameID, Session session) {
+        if (connections.get(gameID) != null) {
+            connections.get(gameID).remove(session);
+        }
     }
 
     public void broadcast() {
